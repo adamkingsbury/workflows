09047ed76c224c3188a6e5d640f609a6af11cfdf
adamk <adam.kingsbury@gmail.com>
2017-11-22 18:35:34 +0000

    added compass scss conversion to workflow

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,44 @@ const gUtil = require('gulp-util');
 const coffee = require('gulp-coffee');
 const concat = require('gulp-concat');
 const browserify = require('gulp-browserify');
+const compass = require('gulp-compass');
 
 var coffeeSources = [
   'components/coffee/tagline.coffee'
-]
+];
 
 var jsSources = [
   'components/scripts/rclick.js',
   'components/scripts/pixgrid.js',
   'components/scripts/tagline.js',
   'components/scripts/template.js'
-]
+];
+
+var sassSources = [
+  'components/sass/style.scss'
+];
 
 gulp.task('coffee', function () {
   gulp.src(coffeeSources)
     .pipe(coffee({ bare: true })
       .on('error', gUtil.log))
     .pipe(gulp.dest('components/scripts'))
-})
+});
 
 gulp.task('js', function () {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
     .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function () {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded'
+    }))
+    .on('error', gUtil.log)
+    .pipe(gulp.dest('builds/development/css'))
 });
